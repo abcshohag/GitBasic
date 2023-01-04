5b6eaf2d27fb574ba4ae15b5326a1c1c5e1d7199
Abu Shohag <abcshoh@gmail.com>
2023-01-03 20:53:35 -0500

    deleted xyz

--- xyz.java
+++
@@ -1,7 +0,0 @@
-public class xyz {
-    public static void main(String[] args) {
-        System.out.println(":adding a second commit");
-        System.out.println(":added button");
-        System.out.println("added something new");
-    }
-}
